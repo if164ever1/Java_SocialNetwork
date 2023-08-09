40c15aec52c722a8ce3c36406ac511fa427b7822
if164ever1 <acere5ihor@gmail.com>
2023-08-09 18:06:07 +0200

    add annotations to comment model

--- src/main/java/entity/Comment.java
+++ src/main/java/entity/Comment.java
@@ -1,15 +1,24 @@
 package entity;
 
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
+@Entity
 public class Comment {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.EAGER)
     private Post post;
+    @Column(nullable = false)
     private String userName;
+    @Column(nullable = false)
     private Long userId;
+    @Column(columnDefinition = "text",nullable = false)
     private String message;
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist
