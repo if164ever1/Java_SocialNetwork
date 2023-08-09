608b0cacffe2e9d0795f798d4d8df2efecbde53e
if164ever1 <acere5ihor@gmail.com>
2023-08-09 17:42:12 +0200

    create image model

--- src/main/java/entity/ImageModel.java
+++ src/main/java/entity/ImageModel.java
@@ -1,4 +1,9 @@
 package entity;
 
 public class ImageModel {
+    private Long imageId;
+    private String name;
+    private byte[] imageByte;
+    private Long userId;
+    private Long postId;
 }
