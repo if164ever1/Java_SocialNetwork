207788a9cbd8e89d877c4c47aaf5882a24c89a28
if164ever1 <acere5ihor@gmail.com>
2023-08-09 18:02:56 +0200

    add annotations to post model

--- src/main/java/entity/Post.java
+++ src/main/java/entity/Post.java
@@ -1,6 +1,7 @@
 package entity;
 
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,16 +10,28 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String caption;
     private String location;
     private Integer likes;
 
+    @Column
+    @ElementCollection(targetClass = String.class)
     private Set<String> likedUsers = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER, mappedBy = "post", orphanRemoval = true)
     private List<Comment> comments = new ArrayList<Comment>();
+
+    @Column(updatable = false)
     private LocalDateTime createdDate;
     @PrePersist
     protected void onCreate(){
