32937ff44ab4b4cc375b0aabf42cd50606130167
if164ever1 <acere5ihor@gmail.com>
2023-08-09 18:16:10 +0200

    add annotations to image model

--- src/main/java/entity/ImageModel.java
+++ src/main/java/entity/ImageModel.java
@@ -1,9 +1,22 @@
 package entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class ImageModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long imageId;
+    @Column(nullable = false)
     private String name;
+    @Lob
+    @Column(columnDefinition = "LONGBLOG")
     private byte[] imageByte;
+    @JsonIgnore
     private Long userId;
+    @JsonIgnore
     private Long postId;
 }
