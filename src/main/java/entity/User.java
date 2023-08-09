6e46f273eefa2a5838836ce00fe589943b3e4492
if164ever1 <acere5ihor@gmail.com>
2023-08-09 14:11:14 +0200

    create usermodel

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,25 @@
 package entity;
 
+import entity.enums.ERole;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class User {
+    private Long id;
+    private String name;
+    private String username;
+    private String lastName;
+    private String email;
+    private String bio;
+    private String password;
+    private final Set<ERole> role = new HashSet<>();
+    private ArrayList<Post> posts = new ArrayList<>();
+    private LocalDateTime createdDate;
+
+    protected void onCreate(){
+        this.createdDate = LocalDateTime.now();
+    }
 }
