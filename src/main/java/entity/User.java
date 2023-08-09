823fe99b90c324539a29797574aa7e57a85aa575
if164ever1 <acere5ihor@gmail.com>
2023-08-09 17:56:21 +0200

    add annotations to user model

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,25 +1,45 @@
 package entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import entity.enums.ERole;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(unique=true, nullable = true)
     private String username;
+    @Column(nullable = false)
     private String lastName;
+    @Column(unique = true)
     private String email;
+    @Column(columnDefinition = "text")
     private String bio;
+    @Column(length = 3000)
     private String password;
+
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn("user_id"))
     private final Set<ERole> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createdDate;
+
+    private Collection<? extends GrantedAuthority> autorities;
 
     @PrePersist
     protected void onCreate(){
