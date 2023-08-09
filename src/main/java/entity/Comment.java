f19deb60bc67e6b09d1fea1fd920b7916b088181
if164ever1 <acere5ihor@gmail.com>
2023-08-09 17:44:20 +0200

    create coment model

--- src/main/java/entity/Comment.java
+++ src/main/java/entity/Comment.java
@@ -1,4 +1,19 @@
 package entity;
 
+import jakarta.persistence.PrePersist;
+
+import java.time.LocalDateTime;
+
 public class Comment {
+    private Long id;
+    private Post post;
+    private String userName;
+    private Long userId;
+    private String message;
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdDate = LocalDateTime.now();
+    }
 }
