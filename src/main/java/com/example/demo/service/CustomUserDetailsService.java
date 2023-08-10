2a97c92797a67bab5fae69b53118d95a10d36ec0
if164ever1 <acere5ihor@gmail.com>
2023-08-10 11:13:15 +0200

    create customuserdetails service class

---
+++ src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -0,0 +1,49 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new User(user.getId(),
+                     user.getUsername(),
+                     user.getEmail(),
+                     user.getPassword(),
+                     (Collection<? extends GrantedAuthority>) getAuthorities(user));
+    }
+
+    public User findUserById(Long id){
+        return userRepository.findUserById(id).orElse(null);
+    }
+
+    private Object getAuthorities(User user) {
+        List<GrantedAuthority> authority = user.getRole().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+
+        return authority;
+    }
+}
