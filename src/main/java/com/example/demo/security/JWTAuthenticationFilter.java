71ca0936fa31ed8b767fbb7780111d8c3e3e5ceb
if164ever1 <acere5ihor@gmail.com>
2023-08-13 15:38:17 +0200

    create JWTAuthentikationFilter  class

---
+++ src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -0,0 +1,55 @@
+package com.example.demo.security;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    public static final Logger LOG = LoggerFactory.getLogger(JWTAuthenticationFilter.class);
+    private JWTTokenProvider tokenProvider;
+    private CustomUserDetailsService userDetailsService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+            String jwt = getJWTFromRequest(request);
+
+            if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+                Long userId = tokenProvider.getUserIdFromToken(jwt);
+                User userDetails = userDetailsService.findUserById(userId);
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }catch (Exception ex){
+            // Handle exceptions and log the error
+            LOG.error("Authentication error: " + ex.getMessage(), ex);
+        }
+    }
+
+    private String getJWTFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+
+        return null; // Return null if no token found
+    }
+}
